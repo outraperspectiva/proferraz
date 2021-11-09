491c50a6c9c527e8dc48998cbf55997b0a44839a
Fábio Ferraz Fernandez <fabio.fernandez@ifb.edu.br>
2021-11-09 17:35:50 -0300

    main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Professor Fábio Ferraz Fernandez
         </p>
         <a
           className="App-link"
