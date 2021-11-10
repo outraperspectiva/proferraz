a958e4cf323c733de30d4eaaa364f851f53f2dbc
Fábio Ferraz Fernandez <fabio.fernandez@ifb.edu.br>
2021-11-09 21:12:29 -0300

    nova app.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.ifb.edu.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Conheça o IFB
         </a>
       </header>
     </div>
